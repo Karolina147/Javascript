e731e3b1e1c7e9447ea18969bbd021ea7a95b548
Karolina147 <54249154+Karolina147@users.noreply.github.com>
2020-03-07 13:42:48 +0100

    draw lots - child name

--- main.js
+++ main.js
@@ -1,45 +1,14 @@
-const input = document.getElementById('pass');
-const div = document.querySelector('.message');
+const btn = document.querySelector('button');
+const names = ['Jagienka', 'Dobrosława', 'Daria', 'Asia', 'Kira', 'Marysia', 'Anastazja'];
+const prefixs = ["Wydaje mi się", "Mam wrażenie", "Szczerze powiedziawszy, myślę", "Szczerze uważam", "Na 100% twierdzę", "Jestem pewien"];
 
-const passwords = ['user', 'wiosna', 'Ania'];
-const messages = ['Wyjechałam na zawsze', 'piękna pora roku', 'fajny styl masz'];
+const div = document.querySelector('div');
 
-input.addEventListener('input', (e) => {
-    div.textContent = '';
-    const text = e.target.value;
-    //console.log(text);
+const nameGenerator = () => {
+    const indexName = Math.floor(Math.random() *names.length);
+    // const indexPrefix = Math.floor(Math.random() *prefixs.length);
 
-    passwords.forEach((password, index) => { // pierwsza zmienna to kolejne wartości elementów tablicy, druga to indeksy tablicy
-        if (password === text) {
-            div.textContent = messages[index];
-            e.target.value = '';
-            
-        } 
-
-    })
-
-})
-
-
-
-
-// input.addEventListener('input', (e) => { // można też użyć 'keyup' - na podniesienie klawisza, jest też 'change' pokaże zmiany w polu, ale ne dla tego zadania
-//     // console.log(e.target.value); // element e. (event), wykonuje się na inpucie i wartość, 
-//     if (password === e.target.value) { // wczesniej był const password - nie passwords - bo jedna zmienna
-//         div.textContent = message;
-//         e.target.value = '';
-
-//     } else {
-//         div.textContent='';
-//     }
-// })
-
-input.addEventListener('focus', (e) => {
-    e.target.classList.add('active'); //można użyć toggle
+    div.innerHTML = `${prefixs[Math.floor(Math.random() *prefixs.length)]}, że najfajniejsze imię to ${names[indexName].bold()}`;
 }
-)
 
-input.addEventListener('blur', (e) => {
-    e.target.classList.remove('active'); //można użyć
-}
-)
+btn.addEventListener('click',  nameGenerator)
