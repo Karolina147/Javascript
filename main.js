cdf029376e4a02c052c46a3f35a52963f62ed239
Karolina147 <54249154+Karolina147@users.noreply.github.com>
2020-03-07 13:41:36 +0100

    enter password box

--- main.js
+++ main.js
@@ -1,36 +1,45 @@
-const  div = document.querySelector('div');
-let divX = 150;
-let divY = 50;
-div.style.left = divX + "px";
-div.style.top = `${divY}px`;
+const input = document.getElementById('pass');
+const div = document.querySelector('.message');
 
+const passwords = ['user', 'wiosna', 'Ania'];
+const messages = ['Wyjechałam na zawsze', 'piękna pora roku', 'fajny styl masz'];
 
-let drawActive = false; // bololean dla rysowania
+input.addEventListener('input', (e) => {
+    div.textContent = '';
+    const text = e.target.value;
+    //console.log(text);
 
-let insertDivX;
-let insertDivY;
+    passwords.forEach((password, index) => { // pierwsza zmienna to kolejne wartości elementów tablicy, druga to indeksy tablicy
+        if (password === text) {
+            div.textContent = messages[index];
+            e.target.value = '';
+            
+        } 
 
+    })
 
-div.addEventListener('mousedown', (e) => { // reakcja na wciśnięcie, insertDivy będą zależały od e
-    div.style.backgroundColor = 'grey';
-    drawActive = true;
-    
-    insertDivX = e.offsetX; // odległość od miejsca kliknięcia do naszego elementu (wewnątrz elementu)
-    insertDivY = e.offsetY;
-    console.log(insertDivX, insertDivY);
 })
 
-div.addEventListener('mousemove', (e) => { // ruszanie myszką, zależne od parametru e
-    if(drawActive) {
-        divX = e.clientX - insertDivX; // e.clientX - informacja o tym gdzie kliknęliśmy względem krawędzi przeglądarki, czli odległośc od przeglądarki - odl. wewnątrz kwadratu
-        divY = e.clientY - insertDivY;
 
-        div.style.left = `${divX}px`;
-        div.style.top = `${divY}px`;
-    }
-})
 
-div.addEventListener('mouseup', () => { // puszczony klawisz mysz
-    div.style.backgroundColor = "black";
-    drawActive = false; // to samo co drawActive != drawActive;
-})
+
+// input.addEventListener('input', (e) => { // można też użyć 'keyup' - na podniesienie klawisza, jest też 'change' pokaże zmiany w polu, ale ne dla tego zadania
+//     // console.log(e.target.value); // element e. (event), wykonuje się na inpucie i wartość, 
+//     if (password === e.target.value) { // wczesniej był const password - nie passwords - bo jedna zmienna
+//         div.textContent = message;
+//         e.target.value = '';
+
+//     } else {
+//         div.textContent='';
+//     }
+// })
+
+input.addEventListener('focus', (e) => {
+    e.target.classList.add('active'); //można użyć toggle
+}
+)
+
+input.addEventListener('blur', (e) => {
+    e.target.classList.remove('active'); //można użyć
+}
+)
